75434bc65df9c5521208e61e67892c0524fbe36e
NAYANJITH <nayanjithajith@gmail.com>
2025-07-05 10:37:23 +0530

    0.1

---
+++ thanal/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './components/Home/Home.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
